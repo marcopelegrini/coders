7e0298f2027cadb39a58b1d27c723e8f6b40079d
marcotuliolp <marcotuliolp@033e6502-5d2e-11de-b7a8-4be610c0df87>
2009-06-26 00:40:55 +0000

    Pegar sistema operacional Resetar preferencias

--- trunk/changeHosts/defaults/preferences/changeHosts.js
+++ trunk/changeHosts/defaults/preferences/changeHosts.js
@@ -3,5 +3,6 @@ pref("extensions.changeHosts.definitions-dir", "");
 pref("extensions.changeHosts.show-in-status", true);
 pref("extensions.changeHosts.definitions-ext",".hosts");
 pref("extensions.changeHosts.flush-dns", true);
+pref("extensions.changeHosts.reload-page", true);
 pref("extensions.changeHosts.show-ip-status", true);
 pref("extensions.changeHosts.show-icon-status", true);
