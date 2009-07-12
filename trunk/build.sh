b8309b6b0fece8afbcdfa8910fa7974e50b97749
marcotuliolp <marcotuliolp@033e6502-5d2e-11de-b7a8-4be610c0df87>
2009-07-12 00:21:07 +0000

    Alteracao build

--- trunk/build.sh
+++ trunk/build.sh
@@ -2,12 +2,16 @@
 if [ $1 ]; then
 	rm -rf target *.xpi
 	svn export $1 target
-	cd target
-	rm -rf .settings .project
-	zip -r9vT $1.xpi *
-	mv $1.xpi ..
-	cd ..
-	rm -rf target
+	if [ -d target ];then
+		cd target
+		rm -rf .settings .project
+		zip -r9vT $1.xpi *
+		mv $1.xpi ..
+		cd ..
+		rm -rf target
+	else
+		echo "Erro ao exportar projeto"
+	fi
 else
 	echo "Informe o projeto"
 fi
