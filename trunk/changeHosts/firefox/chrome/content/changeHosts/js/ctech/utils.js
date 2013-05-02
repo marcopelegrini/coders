629b628e21187a3176d3e89dca74e62588f65e04
marcotuliolp@gmail.com <marcotuliolp@gmail.com@033e6502-5d2e-11de-b7a8-4be610c0df87>
2013-05-02 00:21:41 +0000

    Organizando diretorios

---
+++ trunk/changeHosts/firefox/chrome/content/changeHosts/js/ctech/utils.js
@@ -0,0 +1,91 @@
+
+/**
+ * General utility for log purpose
+ *
+ * @author marcotulio
+ */
+if (!coders) 
+    var coders = {};
+if (!coders.utils) 
+    coders.utils = {};
+
+(function(){
+
+    coders.utils.log = function(preferences){
+    
+        this.logPreference = "log";
+        this.enabled = preferences.getBool(this.logPreference);
+        this.console = Application.console;
+        
+        this.debug = function(string){
+            if (this.enabled) {
+                this.console.log("[DEBUG] - " + string);
+            }
+        }
+        this.info = function(string){
+            if (this.enabled) {
+                this.console.log("[INFO] - " + string);
+            }
+        }
+        this.warn = function(string){
+            if (this.enabled) {
+                this.console.log("[WARN] - " + string);
+            }
+        }
+        this.error = function(string){
+            if (this.enabled) {
+                this.console.log("[ERROR] - " + string);
+            }
+        }
+    }
+    
+    coders.utils.util = {
+    
+        /**
+         * General utilities
+         */
+        getOperationSystem: function(){
+            var sysInfo = Components.classes['@mozilla.org/system-info;1'].getService(Components.interfaces.nsIPropertyBag2);
+            var plataform = sysInfo.getProperty('name');
+            return plataform;
+        },
+        
+        isMacOS: function(){
+            return this.getOperationSystem() == "Darwin";
+        },
+        
+        trim: function(string){
+            if (string != null && string != undefined) {
+                return string.replace(/^\s*/, "").replace(/\s*$/, "");
+            }
+            return string;
+        },
+        
+        getElement: function(id, aDocument){
+            if (aDocument) {
+                return aDocument.getElementById(id);
+            }
+            return document.getElementById(id);
+        },
+        
+        getBrowserWindow: function(){
+            var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+            return wm.getMostRecentWindow("navigator:browser");
+        },
+        
+        getOptionsWindow: function(){
+            var wm = Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator);
+            return wm.getMostRecentWindow("changeHosts:settings");
+        },
+        
+        isBlank: function(string){
+            if (string && string != null && string != undefined) {
+                var newString = this.trim(string);
+                if (newString.length > 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+})();
